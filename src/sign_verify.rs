fa64de23c6f44d251cc504c83161a5dd85d124b1
z4yx <yuxiang.zhang@tuna.tsinghua.edu.cn>
2022-02-09 17:30:38 +0800

    fix ED25519

--- src/sign_verify.rs
+++ src/sign_verify.rs
@@ -2,9 +2,9 @@
 // #[macro_use]
 // extern crate lazy_static;
 // use libc::c_ulonglong;
-use openssl::pkey::{PKey, Public};
+use openssl::ec::{EcGroup, EcKey, EcPoint};
 use openssl::nid::Nid;
-use openssl::ec::{EcGroup,EcPoint,EcKey};
+use openssl::pkey::{PKey, Public};
 use ssh_agent::proto::public_key::PublicKey;
 
 use std::ffi;
@@ -17,13 +17,7 @@ trait ToOpensslKey {
     fn to_pkey(&self) -> Result<PKey<Public>, ErrType>;
 }
 impl ToOpensslKey for PublicKey {
-    fn to_pkey(&self) -> Result<PKey<Public>,ErrType> {
-        let toEcKey = |nid, q:&Vec<u8>| -> Result<PKey<Public>,ErrType> {
-            let group = EcGroup::from_curve_name(nid)?;
-            let mut ctx = openssl::bn::BigNumContext::new()?;
-            let pt = EcPoint::from_bytes(&group, q, &mut ctx)?;
-            Ok(PKey::from_ec_key(EcKey::from_public_key(&group, &pt)?)?)
-        };
+    fn to_pkey(&self) -> Result<PKey<Public>, ErrType> {
         match self {
             PublicKey::EcDsa(input) => {
                 println!("input.identifier={}", input.identifier);
@@ -31,22 +25,28 @@ impl ToOpensslKey for PublicKey {
                     "nistp256" => Nid::X9_62_PRIME256V1,
                     "nistp384" => Nid::SECP384R1,
                     "nistp521" => Nid::SECP521R1,
-                    _ => return Err(RsshErr::PARSE_PUBKEY_ERR.into_ptr())
+                    _ => return Err(RsshErr::PARSE_PUBKEY_ERR.into_ptr()),
                 };
-                toEcKey(nid, &input.q)
-            },
-            // PublicKey::Ed25519(input) => {
-            //     println!("input ed25519");
-            //     toEcKey(Nid::SECP112R1, &input.enc_a)
-            // },
+                let group = EcGroup::from_curve_name(nid)?;
+                let mut ctx = openssl::bn::BigNumContext::new()?;
+                let pt = EcPoint::from_bytes(&group, q, &mut ctx)?;
+                Ok(PKey::from_ec_key(EcKey::from_public_key(&group, &pt)?)?)
+            }
+            PublicKey::Ed25519(input) => {
+                println!("input ed25519");
+                Ok(PKey::public_key_from_raw_bytes(
+                    &input.enc_a,
+                    openssl::pkey::Id::ED25519,
+                )?)
+            }
             PublicKey::Rsa(input) => {
                 use openssl::bn::BigNum;
                 use openssl::rsa::Rsa;
                 let e = BigNum::from_slice(&input.e)?;
                 let n = BigNum::from_slice(&input.n)?;
-                Ok(PKey::from_rsa(Rsa::from_public_components(n,e)?)?)
-            },
-            _ => Err(RsshErr::PARSE_PUBKEY_ERR.into_ptr())
+                Ok(PKey::from_rsa(Rsa::from_public_components(n, e)?)?)
+            }
+            _ => Err(RsshErr::PARSE_PUBKEY_ERR.into_ptr()),
         }
     }
 }
@@ -60,13 +60,16 @@ pub fn verify_signature(
     pubkey: &PublicKey,
     signature: &[u8],
 ) -> Result<bool, ErrType> {
-    use openssl::sign::Verifier;
     use openssl::hash::{hash, MessageDigest};
+    use openssl::sign::Verifier;
 
     let pkey = pubkey.to_pkey()?;
-    let digest = hash(MessageDigest::sha256(), content)?;
-    let mut verifier = Verifier::new_without_digest(&pkey)?;
-    let ret = verifier.verify_oneshot(signature, &digest)?;
+    // let digest = hash(MessageDigest::sha256(), content)?;
+    let mut verifier = match pubkey{
+        PublicKey::Ed25519(_) => Verifier::new_without_digest(&pkey),
+        _ => Verifier::new(MessageDigest::sha256(), &pkey),
+    }?;
+    let ret = verifier.verify_oneshot(signature, &content)?;
 
     Ok(ret)
 }
